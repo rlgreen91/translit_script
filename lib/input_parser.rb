d061f04db573c8b5a2485729a71987b41757686b
Rachel Green <rlgreen91@gmail.com>
2015-10-18 01:49:58 -0500

    Parser parses 'a'

--- lib/input_parser.rb
+++ lib/input_parser.rb
@@ -3,12 +3,21 @@ include Parslet
 
 #Parser for transliteration from english to japanese
 class InputParser < Parslet::Parser
+	#stop parse if there is a "q" or "v" or characters other than letters
+	str('[^A-PR-UW-Za-pr-uw-z]').absent?
+
 	#check for vowels
-	rule(:vowel) { match('a') | match('e') | match('i') | match('o') | match('u')}
+	rule(:a) { match('a') }
+	rule(:i) { match('i') }
+	rule(:u) { match('u') }
+	rule(:e) { match('e') }
+	rule(:o) { match('o') }
+
+	rule(:vowel) { ( a.as(:a) | i.as(:i) | u.as(:u) | e.as(:e) | o.as(:o) ).repeat(1) }
 
 	#Parse expression
 	#check for vowels
-	rule(:expression) { vowel.repeat(1) }
+	rule(:expression) { vowel }
 	root :expression
 end
 
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -12,5 +12,27 @@ RSpec.describe "test parser input type" do
 			expect{@string_input.parse('eie')}.not_to raise_error
 			expect{@string_input.parse('ieieoueiau')}.not_to raise_error
 		end
-	end	
+	end
+
+	context "input has q and/or v and/or characters other than letters" do
+		it "raises ParseFailed" do
+			expect{@string_input.parse("quiet")}.to raise_error(ParseFailed)
+			expect{@string_input.parse("invalid input")}.to raise_error(ParseFailed)
+			expect{@string_input.parse("I'm")}.to raise_error(ParseFailed)
+		end	
+	end
+end
+
+RSpec.describe "test vowels" do
+	before(:all) do
+		@string_input = InputParser.new
+	end
+
+	context "only one vowel" do
+		it "parses succesfully" do
+			expected = '[{:a=>"a"@0}]'
+			expect(@string_input.parse('a').to_s).to eq(expected)
+		end
+	end
+
 end
